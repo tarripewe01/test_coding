324ade1d8d354c4f3b8a00ad936042f2d5882ab9
tarripewe <tarripewe01@gmail.com>
2022-12-10 14:10:38 +0700

    Palindrome Recursive

--- src/test/Palindrome.js
+++ src/test/Palindrome.js
@@ -1,15 +1,32 @@
-const palindrome = (string) => {
-  const len = string.length;
+// const palindrome = (string) => {
+//   //   const len = string.length;
 
-  for (let i = 0; i <= len; i++) {
-    if (string[i] !== string[len - i - 1]) {
+//   //   for (let i = 0; i <= len / 2; i++) {
+//   //     if (string[i] !== string[len - i - 1]) {
+//   //       return false;
+//   //     }
+//   //   }
+//   //   return true;
+
+// };
+
+// menggunakan recursive
+const palindromeRecursive = (string, i) => {
+  if (i < string.length / 2) {
+    if (string[i] !== string[string.length - i - 1]) {
       return false;
+    } else {
+      return palindromeRecursive(string, i + 1);
     }
+  } else {
+    return true;
   }
-  return true;
 };
 
+// console.log(palindrome("kita")); // false
+// console.log(palindrome("isi")); // true
+// console.log(palindrome("katak")); // true
 
-console.log(palindrome("kita")) // false
-console.log(palindrome("isi")) // true
-console.log(palindrome("katak")) // true
+console.log(palindromeRecursive("kita", 0)); // false
+console.log(palindromeRecursive("isi", 0)); // true
+console.log(palindromeRecursive("katak", 0)); // true
