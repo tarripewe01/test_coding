3731d15ca8070651e90859c5d8042f964941920a
tarripewe <tarripewe01@gmail.com>
2022-12-10 19:24:22 +0700

    -

--- src/components/Searchable.js
+++ src/components/Searchable.js
@@ -3,11 +3,13 @@ import Select from "react-select";
 
 const Searchable = () => {
   const [data, setData] = useState([]);
+  const [allData, setAllData] = useState([]);
   const [dataSelected, setDataSelected] = useState("");
 
   const getData = async () => {
     const allData = await fetch("https://pokeapi.co/api/v2/berry/");
     const value = await allData.json();
+    // console.log(value.results)
     const result = await value.results.map((select) => {
       return {
         label: select.name,
@@ -16,6 +18,7 @@ const Searchable = () => {
     });
     const finalResult = result.sort((a, b) => a.label.localeCompare(b.label));
     setData(finalResult);
+    setAllData(value.results);
   };
 
   useEffect(() => {
@@ -31,6 +34,13 @@ const Searchable = () => {
       <h1>Searchable Option</h1>
       <Select options={data} onChange={handleChange} />
       <h2>{dataSelected}</h2>
+      {/* {allData.map((item, idx) => {
+        return (
+          <div key={idx}>
+            <h3>{item.name}</h3>
+          </div>
+        );
+      })} */}
     </div>
   );
 };
